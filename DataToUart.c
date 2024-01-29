b86b76f9d81daf4930b3b91c12863a596401e8c8
sadek <mhmod.sadek2017@gmail.com>
2024-01-29 04:30:36 +0200

    "First commit"

---
+++ DataToUart.c
@@ -0,0 +1,15 @@
+
+#include <stdint.h>
+
+
+
+char Array[] = "In a General-Purpose Computer running many processes simultaneously,\
+		 we need a middle layer to manage the distribution of the computer’s hardware resources efficiently\
+		 and fairly among all the various processes running on the computer.\
+		 This middle layer is referred to as the kernel.\
+		 The kernel virtualizes the computer’s common hardware resources to provide each process with its own virtual resources.\
+		 This makes the process seem as if it is the sole process running on the machine.\
+		 The kernel is also responsible for preventing and mitigating conflicts between different processes";
+
+
+uint16_t Length = sizeof(Array);
